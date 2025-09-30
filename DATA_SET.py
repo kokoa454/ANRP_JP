87b2f5e2cffd6868b95dc0b120c9f31f6ed6e116
Inari Goro <qqnq7um9k@gmail.com>
2025-09-30 12:17:31 +0900

    added DATA_SET

---
+++ DATA_SET.py
@@ -0,0 +1,12 @@
+__package__ = "DATA_SET"
+
+import LICENSE_PLATE
+import fiftyone.zoo as foz
+
+class DATA_SET:
+    DATA_SET_IMAGES_DIR = "./data_set/images"
+    DATA_SET_LABELS_DIR = "./data_set/labels"
+
+    def downloadBackgroundImages():
+        
+    def createDataSet():
