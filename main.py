c71f76796aa1f74787ff93dfec09398ee138df77
Inari Goro <qqnq7um9k@gmail.com>
2025-09-30 14:48:35 +0900

    updated main to show more options

--- main.py
+++ main.py
@@ -3,30 +3,79 @@ import DATA_SET
 
 def main():
     while True:
+        print("ANRP_JP\n")
+        print("""\n作業番号 (
+        0: ナンバープレート生成
+        1: データセット生成
+        2: 終了
+        )\n""")
+
         try:
-            trainingNumber = int(input("車両数?: "))
+            selectedNum = int(input("作業番号?: "))
+        except ValueError:
+            print("数字を入力してください。\n")
+            continue
+
+        print("\n")
+
+        if selectedNum == 0:
+            try:
+                trainingNumber = int(input("ナンバープレート数?: "))
+            except ValueError:
+                print("数字を入力してください。\n")
+                continue
+
+            print("""
+                \n車種 (
+                0: 普通（自家用）
+                1: 普通（事業用）
+                2: 軽（自家用）
+                3: 軽（事業用）
+            )\n""")
+            
+            try:
+                typeOfVehicle = int(input("車種?: "))
+                if typeOfVehicle not in [0, 1, 2, 3]:
+                    print("0~3の数字を入力してください。\n")
+                    continue
+            except ValueError:
+                print("数字を入力してください。\n")
+                continue
+
+            LICENSE_PLATE.LICENSE_PLATE(trainingNumber, typeOfVehicle)
+            print("\n")
+
+        elif selectedNum == 1:
+            try:
+                trainingNumber = int(input("データセット数?: "))
+            except ValueError:
+                print("数字を入力してください。\n")
+                continue
+
+            print("""
+                \n車種 (
+                0: 普通（自家用）
+                1: 普通（事業用）
+                2: 軽（自家用）
+                3: 軽（事業用）
+            )\n""")
+            
+            try:
+                typeOfVehicle = int(input("車種?: "))
+                if typeOfVehicle not in [0, 1, 2, 3]:
+                    print("0~3の数字を入力してください。\n")
+                    continue
+            except ValueError:
+                print("数字を入力してください。\n")
+                continue
+
+            DATA_SET.DATA_SET(trainingNumber, typeOfVehicle)
+            print("\n")
+
+        elif selectedNum == 2:
             break
-        except ValueError:
-            print("数字を入力してください。\n")
-
-    print("""
-        \n車種 (
-        0: 普通（自家用）
-        1: 普通（事業用）
-        2: 軽（自家用）
-        3: 軽（事業用）
-    )\n""")
     
-    while True:
-        try:
-            typeOfVehicle = int(input("車種は?: "))
-            if typeOfVehicle in [0, 1, 2, 3]:
-                break
-            else:
-                print("0~3の数字を入力してください。\n")
-        except ValueError:
-            print("数字を入力してください。\n")
-
-    LICENSE_PLATE.LICENSE_PLATE(trainingNumber, typeOfVehicle)
+        else:
+            print("0~2の数字を入力してください。\n")
 
 main()
